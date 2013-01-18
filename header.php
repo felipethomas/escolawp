a646d0f34761a0158a5d1155f75da73fdcc99293
root <root@isidsv01.(none)>
2013-01-18 17:57:33 -0300

    aplicacao de conhecimentos do tutorial da site escolawp

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+ 
+<body>
+<div id="wrapper" class="hfeed">
+    <div id="header">
+        <div id="masthead">
+ 
+            <div id="branding">
+            </div><!– #branding –>
+ 
+            <div id="access">
+            </div><!– #access –>
+ 
+        </div><!– #masthead –>
+    </div><!– #header –>
+ 
+    <div id="main">
